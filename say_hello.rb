33e62764f23c0b9042ba4f6803c525c2d0b81172
Beau Heffron <beauheffron7@aol.com>
2015-10-13 14:52:11 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,9 @@
 # Build your say_hello method here
+name = "Ruby Programmer"
+def say_hello(name)
+  if name == null
+    name = "Ruby Programmer"
+  end
+  puts "Hello #{name}!"
+end
+say_hello(name)
